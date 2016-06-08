4be92123a131a25e33552c6dcd1b517d6b944f58
amyleewalton <amylee@mapbox.com>
2016-06-07 23:23:44 -0400

    no errors and no rendered elements

--- bundle.js
+++ bundle.js
@@ -32885,13 +32885,7 @@ var fs = require('fs');
 var mapboxgl = require('mapbox-gl');
 var generate = require('./generate.js');
 var style = require('../assets/style-point.json');
-// var style = require('../assets/style-line.json');
-// var style = require('../assets/style-poly.json');
-// var style = require('../assets/satellite-streets-v9.json');
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg';
-
-// generate new data source based on style
+// run function from required file to generate data
 var pointdata = generate(style);
 
 // modify the style's sources object
@@ -32903,11 +32897,13 @@ style.sources['newdata'] = {
 // change all the layer refs to the new data source
 style.layers.forEach(function(layer) {
   if(layer.source !== undefined) {
+    // console.log(layer['source-layer']);
+    delete layer['source-layer'];
     layer.source = 'newdata';
   }
 });
 
-// initalize the map
+mapboxgl.accessToken = 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg';
 var map = new mapboxgl.Map({
   container: 'map',
   style: style,
@@ -32915,9 +32911,4 @@ var map = new mapboxgl.Map({
   zoom: 3,
   center: [-102.30,33.43]
 });
-
-// // map.on('load', function () {
-// // });
-
-
 },{"../assets/style-point.json":1,"./generate.js":184,"fs":3,"mapbox-gl":72}]},{},[184,185]);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ var fs = require('fs');
 var mapboxgl = require('mapbox-gl');
 var generate = require('./generate.js');
 var style = require('../assets/style-point.json');
-// var style = require('../assets/style-line.json');
-// var style = require('../assets/style-poly.json');
-// var style = require('../assets/satellite-streets-v9.json');
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg';
-
-// generate new data source based on style
+// run function from required file to generate data
 var pointdata = generate(style);
 
 // modify the style's sources object
@@ -20,11 +14,13 @@ style.sources['newdata'] = {
 // change all the layer refs to the new data source
 style.layers.forEach(function(layer) {
   if(layer.source !== undefined) {
+    // console.log(layer['source-layer']);
+    delete layer['source-layer'];
     layer.source = 'newdata';
   }
 });
 
-// initalize the map
+mapboxgl.accessToken = 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg';
 var map = new mapboxgl.Map({
   container: 'map',
   style: style,
@@ -32,7 +28,3 @@ var map = new mapboxgl.Map({
   zoom: 3,
   center: [-102.30,33.43]
 });
-
-// // map.on('load', function () {
-// // });
-
