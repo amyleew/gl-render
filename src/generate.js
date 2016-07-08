7d9ad1586fe551e5a3ac6986593c03e180f49d1e
amyleewalton <amylee@mapbox.com>
2016-07-08 18:49:02 -0400

    testing slider

--- src/generate.js
+++ src/generate.js
@@ -1,188 +1,437 @@
 var fs = require('fs');
-var _ = require('underscore'); // client-side only (browsers)
-var style = require('./index.js'); //
+var _ = require('underscore');
+var SphericalMercator = require('sphericalmercator');
 
-function generate(style) { // creategeojson data file
+// By default, precomputes up to z30
+var merc = new SphericalMercator({
+    size: 256
+});
+
+// styles to turn into data
+var style = require('../assets/_gl-styles/streets-v9.json');
+var title = style.name;
+// general style elements
+var allFonts = [];  // points
+var hasFonts;
+var fontCount;
+var allColors = [];  // polygons
+var hasColors;
+var colorCount;
+var allFills = {}; // polygons
+var fillCount;
+
+var generate = function (style) { // creategeojson data file
   var allLayers = style.layers;
   var pointLayers = [];
   var polyLayers = [];
   var lineLayers = [];
-  // starting lng and lat for points
-  var startLng_point = -122.6431;
-  var startLat_point = 48.3543;
+  // title of the map style points
+  var titleCoors = merc.forward([-131.8359, 60.2398]);
+  var titleLat = -131.8359;
+  var titleLng = 60.2398;
+  console.log(titleCoors);
+
+  // change value to add for subhead
+  var changeSectionLat = 55.5078; // add values ?
+  var changeSectionLng = -17.5;
+  var changeFirstItemLng = -6;
+  var changeItemLng = -5.5;
+  var changeItemLat = 6;
+
+  // starting lat and lng for points
   var minusLat_point = 0.5;
   var newLat_point;
-  // starting lng and lat for linestring
-  var startLng_line1 = -118.3008;  // right side
-  var startLng_line2 = -110.2148;  // right side
-  var startLat_line = 49.8380;  // left side
-  var minusLat_line = -0.5;
-  var newLat_line;
-  // starting lng and lat for linestring
-  var polyLng1 = -108.6328;  // same on top and bottom end coords
-  var polyLat1 = 48.2832;  // same as lat4
-  var polyLat2 = 50.9584;  // same as lat3
-  var polyLng2 = -104.0625; // same as lng4
-  var newPolyLng1;
-  var newPolyLat1;
-  var newPolyLat2;
-  var plusPolyLng1 = -0.7031; // add
-  var minusPolyLat1 = 1.4832; // subtract
-  var minusPolyLat2 = 1.5819; // subtract
+  var newLng_point;
+  var newFirstLng_point;
+  var newSectionLng_point;
+
+  // starting lng and lat for color polygon
+  var changeFirstColorLng = -12;
+  var changeColorLat = 6;
+  var changeColorLng = 3;
+  var changeColorSectionLng = -2;
+  var colorLng2 = titleLng + changeSectionLng; // same for lng3
+  var colorLat2 = titleLat + changeSectionLat; // same for lat1
+  var colorLng1 = colorLng2 + changeSectionLng;  // same for lng4
+  var colorLat3 = colorLat2 + changeColorLat; // same for lat4
+  var newColorLng2;
+  var newColorLat2;
+  var newColorLng1;
+  var newColorLat3;
 
   var geojson = {
     "type": "FeatureCollection",
       "features": []
     };
 
-  allLayers.forEach(function(layer) { // let's access each layer
+  allFills = { // pass the fill color and name
+    'layers': [
+    ]
+  };
+
+  // plot the name / title of the style
+  feature = {
+    'type': 'Feature',
+    'geometry': {
+      'type': 'Point',
+      'coordinates': [
+        titleLat,
+        titleLng
+      ]
+    },
+    'properties': {
+      'name': title
+    }
+  };
+  geojson.features.push(feature);
+
+  allLayers.forEach(function(layer) { // let's access each layer to gather style info
     if(layer.type !== undefined) {
-      if(layer.type === 'symbol') { // only for symbols
-        pointLayers.push({  // collect type and filter
-          'id': layer.id,
-          'type': layer.type,
-          'source-layer': layer['source-layer']
-        });
-      }
-      if(layer.type === 'line') { // only for lines
-        lineLayers.push({  // collect type and filter
-          'id': layer.id,
-          'type': layer.type,
-          'source-layer': layer['source-layer']
-        });
+      // only symbols
+      if(layer.type === 'symbol') {
+        if(layer.layout['text-font'] !== undefined) { // only show typeset symbols
+          if(layer.layout['text-font'][0] !== undefined) { // show the first font
+            allFonts.push(layer.layout['text-font'][0]);
+          }
+        }
       }
       if(layer.type === 'fill') { // only for polygons
-        polyLayers.push({  // collect type and filter
-          'id': layer.id,
-          'type': layer.type,
-          'source-layer': layer['source-layer']
-        });
+        if(_.isObject(layer.paint['fill-color'])) { // these are objects
+          var values = _.flatten(layer.paint['fill-color'].stops);
+          for(n=0; n < values.length; n++) {
+            if(_.isString(values[n])) {
+              allColors.push(values[n]);
+            }
+          }
+        }
+        else {
+          allColors.push(layer.paint['fill-color']);
+        }
+        if(_.isObject(layer.paint['fill-color'])) {
+          var values2 = _.flatten(layer.paint['fill-color'].stops);
+          for(p=0; p < values2.length; p++) {
+            if(_.isString(values2[p])) {
+              fillLayer = {
+                'name': layer.id,
+                'type': layer['source-layer'],
+                'fill-color': values2[p]
+              };
+              allFills.layers.push(fillLayer);
+            }
+          }
+        } else {
+            fillLayer = {
+              'name': layer.id,
+              'type': layer['source-layer'],
+              'fill-color': layer.paint['fill-color']
+            };
+            allFills.layers.push(fillLayer);
+          }
       }
-    } else {
-      if(layer.ref) {
-        var allLayersIndexbyRef = _.indexBy(allLayers, 'id'); // find a layer from all the layers from its id
-        var refLayer = allLayersIndexbyRef[layer.ref];
-        if(refLayer.type === 'symbol') { // only for symbols
-          pointLayers.push({  // collect type and filter
-            'id': layer.id,
-            'type': refLayer.type,
-            'source-layer': refLayer['source-layer']
-          });
-        }
-        if(refLayer.type === 'line') { // only for lines
-          lineLayers.push({  // collect type and filter
-            'id': layer.id,
-            'type': refLayer.type,
-            'source-layer': refLayer['source-layer']
-          });
-        }
-        if(refLayer.type === 'fill') { // only for polygons
-          polyLayers.push({  // collect type and filter
-            'id': layer.id,
-            'type': refLayer.type,
-            'source-layer': refLayer['source-layer']
-          });
-        }
-      }
+
     }
   });
+  // console.log(allFills);
+  // console.log(allFills.layers);
+  allFonts = _.uniq(allFonts); // doesn't send everything
 
-  // add points
-  if(pointLayers !== undefined || pointLayers !== null) {
-    pointLayers.forEach(function(layer, i) {
-      // console.log('we have points');
-      newLat_point = startLat_point - (minusLat_point * i);
-      feature = {
-        'type': 'Feature',
-        'geometry': {
-          'type': 'Point',
-          'coordinates': [
-            startLng_point,
-            newLat_point
-          ]
-        },
-        'properties': {
-          'name': layer.id,
-          'element': layer.id,
-          'category': layer['source-layer']
-        }
-      };
-      geojson.features.push(feature);
-    });
-  }
-  // add lines
-  if(lineLayers !== undefined || lineLayers !== null) {
-    lineLayers.forEach(function(layer, i) {
-      // console.log('we have lines');
-      newLat_line = startLat_line + (minusLat_line * i);
-      feature = {
-        'type': 'Feature',
-        'geometry': {
-          'type': 'LineString',
-          'coordinates': [
-            [
-              startLng_line1,
-              newLat_line
-            ],
-            [
-              startLng_line2,
-              newLat_line
+  // add typeface collection
+  if(allFonts !== undefined || allFonts !== null) {
+    hasFonts = true;
+    fontCount = allFonts.length;
+    allFonts.forEach(function(font, i) {
+      // section title and first item go here
+      if(i === 0) {
+        newSectionLng_point = titleLng + changeSectionLng;
+        newFirstLng_point = newSectionLng_point + changeFirstItemLng;
+        feature = {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [
+              titleLat,
+              newSectionLng_point
             ]
-          ]
-        },
-        'properties': {
-          'element': layer.id,
-          'category': layer['source-layer']
-        }
-      };
-      geojson.features.push(feature);
-    });
-  // add polygons
-  if(polyLayers !== undefined || polyLayers !== null) {
-    polyLayers.forEach(function(layer, i) {
-      // console.log('we have polys');
-      newPolyLng1 = polyLng1 + (plusPolyLng1 * i);
-      newPolyLat1 = polyLat1 - (minusPolyLat1 * i);
-      newPolyLat2 = polyLat2 - (minusPolyLat2 * i);
-      feature = {
-        'type': 'Feature',
-        'geometry': {
-          'type': 'Polygon',
-          'coordinates': [
-            [
-              [
-                newPolyLng1,
-                newPolyLat1
-              ],
-              [
-                newPolyLng1,
-                newPolyLat2
-              ],
-              [
-                polyLng2,
-                newPolyLat2
-              ],
-              [
-                polyLng2,
-                newPolyLat1
-              ],
-              [
-                newPolyLng1,
-                newPolyLat1
-              ]
-            ]
-          ]
-        },
-        'properties': {
-          'element': layer.id,
-          'category': layer['source-layer']
+          },
+          'properties': {
+            'name': 'Typeface'
           }
         };
         geojson.features.push(feature);
+        feature = {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [
+              titleLat,
+              newFirstLng_point
+            ]
+          },
+          'properties': {
+            'name': font,
+            'text-field': font,
+            'text-font': font
+          }
+        };
+        geojson.features.push(feature);
+      } else {
+        newLng_point = newFirstLng_point + (changeItemLng*i);
+        // STORE TYPEFACE STUFF AS FEATURE (NEXT COLORS ... right below it)
+        feature = {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [
+              titleLat,
+              newLng_point
+            ]
+          },
+          'properties': {
+            'name': font,
+            'text-field': font,
+            'text-font': font
+          }
+        };
+      }
+      geojson.features.push(feature);
       });
-      return geojson;
-    }
+  } else {
+    hasFonts = false;
   }
-}
+  // add color collection
+  if(allColors !== undefined || allColors !== null) {
+    allColors = _.uniq(allColors);
+    colorCount = allColors.length;
+    allColors.forEach(function(color, j) {
+      // section title and first item go here
+      if(j === 0) {
+        newSectionLng_point = titleLng + changeSectionLng;
+        colorLng1 = newSectionLng_point + changeFirstColorLng;
+        colorLng2 =  newFirstLng_point + changeColorLng;
+        feature = {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Point',
+            'coordinates': [
+              colorLat2,
+              newSectionLng_point
+            ]
+          },
+          'properties': {
+            'name': 'Colors'
+          }
+        };
+        geojson.features.push(feature);
+        feature = {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Polygon',
+            'coordinates': [
+              [
+                [
+                  colorLat2,
+                  colorLng1
+                ],
+                [
+                  colorLat2,
+                  colorLng2
+                ],
+                [
+                  colorLat3,
+                  colorLng2
+                ],
+                [
+                  colorLat3,
+                  colorLng1
+                ],
+                [
+                  colorLat2,
+                  colorLng1
+                ]
+              ]
+            ]
+          },
+          'properties': {
+            'fill-color': color,
+            'name': color
+          }
+        };
+        geojson.features.push(feature);
+      } else {
+        newColorLat2 = colorLat2 + (changeColorLat*j);
+        newColorLat3 = colorLat3 + (changeColorLat*j);
+        feature = {
+          'type': 'Feature',
+          'geometry': {
+            'type': 'Polygon',
+            'coordinates': [
+              [
+                [
+                  newColorLat2,
+                  colorLng1
+                ],
+                [
+                  newColorLat2,
+                  colorLng2
+                ],
+                [
+                  newColorLat3,
+                  colorLng2
+                ],
+                [
+                  newColorLat3,
+                  colorLng1
+                ],
+                [
+                  newColorLat2,
+                  colorLng1
+                ]
+              ]
+            ]
+          },
+          'properties': {
+            'fill-color': color,
+            'name': color
+          }
+        };
+        geojson.features.push(feature);
+      }
+    });
+  } else {
+      hasColors = false;
+    }
+    if(allFills !== undefined || allFills !== null) {
+      var sorted = {};
+      Object.keys(allFills.layers).forEach(function(key) {
+        var fill = allFills.layers[key];
+        var type = fill.type;
+        if (!sorted[type]) {
+          sorted[type] = [fill];
+        } else {
+          sorted[type].push(fill);
+        }
+      });
+      // save the new sections
+      var eachType = [];
+      allFills.layers.forEach(function(type, i) {
+        eachType.push(type.type);
+      });
+      // for every object in sorted, place a section title layer (find each key)
+      var newSection = false;
+      var thisSection = -1;
+      for (var section in sorted) { // loop thru each section
+        newSection = true;  // this is a new section
+        thisSection ++;
+        // console.log('start new section number ' + thisSection + ', section ' + section);
+        titleLng = titleLng + changeSectionLng;
+        // console.log('start at lng ' + titleLng);
+        Object.keys(sorted[section]).forEach(function(key, i) { // find the keys in each section
+          if(i === 0) {
+            newSectionLng_point = titleLng + changeSectionLng;
+            colorLng1 = newSectionLng_point + changeSectionLng+3; // lowest point
+            colorLng2 = newSectionLng_point + changeColorSectionLng;
+            feature = {
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Point',
+                'coordinates': [
+                  colorLat2,
+                  newSectionLng_point
+                ]
+              },
+              'properties': {
+                'name': sorted[section][key].type
+              }
+            };
+            geojson.features.push(feature);
+            feature = {
+              'type': 'Feature',
+              'geometry': {
+                'type': 'Polygon',
+                'coordinates': [
+                  [
+                    [
+                      colorLat2,
+                      colorLng1
+                    ],
+                    [
+                      colorLat2,
+                      colorLng2
+                    ],
+                    [
+                      colorLat3,
+                      colorLng2
+                    ],
+                    [
+                      colorLat3,
+                      colorLng1
+                    ],
+                    [
+                      colorLat2,
+                      colorLng1
+                    ],
+                  ]
+                ]
+              },
+            'properties': {
+              'name': sorted[section][key].name,
+              'type': sorted[section][key].type,
+              'fill-color': sorted[section][key]['fill-color']
+            }
+          };
+          geojson.features.push(feature);
+        } else { // same lat just down a row
+          newSection = false; // also we are not making new section fills
+          newColorLat2 = colorLat2 + (changeColorLat*i);
+          newColorLat3 = colorLat3 + (changeColorLat*i);
+          feature = {
+            'type': 'Feature',
+            'geometry': {
+              'type': 'Polygon',
+              'coordinates': [
+                [
+                  [
+                    newColorLat2,
+                    colorLng1
+                  ],
+                  [
+                    newColorLat2,
+                    colorLng2
+                  ],
+                  [
+                    newColorLat3,
+                    colorLng2
+                  ],
+                  [
+                    newColorLat3,
+                    colorLng1
+                  ],
+                  [
+                    newColorLat2,
+                    colorLng1
+                  ],
+                ]
+              ]
+            },
+          'properties': {
+            'name': sorted[section][key].name,
+            'type': sorted[section][key].type,
+            'fill-color': sorted[section][key]['fill-color']
+          }
+        };
+        geojson.features.push(feature);
+      }
+    });
+  }
+
+  }
+  return {
+    geojson: geojson,
+    allFonts: allFonts,
+    name: title,
+    allColors: allColors,
+    allFills: allFills
+  };
+};
 
 module.exports = generate;
--- src/index.js
+++ src/index.js
@@ -1,101 +1,287 @@
 var fs = require('fs');
 var mapboxgl = require('mapbox-gl');
-
+var _ = require('underscore');
 // styles to turn into data
-var button = document.getElementById('theButton');
-var user, styleID, accessToken, style;
+var style = require('../assets/_gl-styles/streets-v9.json');
+// running function for data
+var generatedData = require('./generate.js')(style);
 
-button.addEventListener('click', function() {
-  user = document.getElementById('user').value;
-  styleID = document.getElementById('styleID').value;
-  accessToken = document.getElementById('accessToken').value;
-  if(user === '') {
-    alert('Input your Mapbox username, then click submit.');
-  } else if(styleID === '') {
-    alert('Input the styleID for the map you wish to see.');
-  } else if(accessToken === '') {
-    alert('Input your Mapbox accessToken.');
-  } else {
-      var styleJSON = 'https://api.mapbox.com/styles/v1/' + user + '/' + styleID + '?access_token=' + accessToken;
-      loadJSON(styleJSON,
-         function(styleParse) {
-          // console.log('but we do have ' + styleParse);
-          style = styleParse;
-          addMap(style);
-        },
-         function(xhr) { console.error(xhr); }
-      );
+// generate new data from style.json
+var generate_data = generatedData.geojson; //Generate.generate(style);
+// modify the style's sources object
+style.sources = {
+  'newdata': {
+    'type': 'geojson',
+    'data': generate_data
+  }
+};
+// change all the layer refs to the new data source
+style.layers.forEach(function(layer) {
+  if(layer.source !== undefined) {
+    delete layer['source-layer'];
+    delete layer.minzoom;
+    delete layer.maxzoom;
+    // delete layer.filter;
+    layer.filter = [
+      '==',
+      'element',
+      layer.id
+    ];
+    layer.source = 'newdata';
+    if(layer.type == 'symbol') { // style names
+      layer.layout['text-field'] = layer.id;
     }
+  }
+});
+
+var showSlider = document.getElementById('slider');
+showSlider.className = 'center';
+
+// console.log(generate_data.features);
+// console.log(style);
+// console.log(Generate);
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibXNsZWUiLCJhIjoiclpiTWV5SSJ9.P_h8r37vD8jpIH1A6i1VRg';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: style,
+  hash: true,
+  zoom: 2,
+  center: [25.4,-34.9]
+});
+
+map.on('load', function() {
+  location.hash = '2/25.4,-34.9';
+  // add non-styled generate data
+  map.addLayer({
+    'id': 'styleTitle',
+    'type': 'symbol',
+    'source': 'newdata',
+    'layout': {
+      'text-field': generatedData.name,
+      'text-font': [generatedData.allFonts[0], generatedData.allFonts[1]],
+      'text-size': 45,
+      'text-anchor': 'left'
+    },
+    'filter': [
+      '==',
+      'name',
+      generatedData.name
+    ]
   });
 
-// now generate the new style.json
-function addMap() {
-  // run function only when we have all the values
-  var generate = require('./generate.js');
-  // generate new data from style.json
-  var generate_data = generate(style);
-  // modify the style's sources object
-  style.sources = {
-    'newdata': {
-      'type': 'geojson',
-      'data': generate_data
-    }
-  };
-  // change all the layer refs to the new data source
-  style.layers.forEach(function(layer) {
-    if(layer.source !== undefined) {
-      delete layer['source-layer'];
-      delete layer.minzoom;
-      delete layer.maxzoom;
-      // delete layer.filter;
-      layer.filter = [
+  // console.log(generatedData);
+  // console.log(generatedData.allFills);
+
+  // style all typefaces used
+  var onlyFonts = _.uniq(generatedData.allFonts);
+  // add section label
+  if (onlyFonts.length > 0) {
+    map.addLayer({
+      'id': 'section',
+      'type': 'symbol',
+      'source': 'newdata',
+      'layout': {
+        'text-field': 'Typeface',
+        'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+        'text-size': 12,
+        'text-anchor': 'left'
+      },
+      'paint': {
+        'text-opacity': 0.7
+      },
+    'filter': [
+      '==',
+      'name',
+      'Typeface'
+    ]
+    });
+  }
+  onlyFonts.forEach(function (font, i) {
+    // add non-styled generatedData data
+    map.addLayer({
+      'id': font,
+      'type': 'symbol',
+      'source': 'newdata',
+      'layout': {
+        'text-field': font,
+        'text-font': [font, 'Arial Unicode MS Regular'],
+        'text-size': 25,
+        'text-anchor': 'left'
+      },
+      'filter': [
         '==',
-        'element',
-        layer.id
-      ];
-      layer.source = 'newdata';
-      if(layer.type == 'symbol') { // style names
-        layer.layout['text-field'] = layer.id;
-      }
+        'name',
+        font
+      ]
+    });
+  });
+
+  // style all colors used
+  var onlyColors = _.uniq(generatedData.allColors);
+  // add section label
+  if (onlyColors.length > 0) {
+    map.addLayer({
+      'id': 'sectionColors',
+      'type': 'symbol',
+      'source': 'newdata',
+      'layout': {
+        'text-field': 'Colors',
+        'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+        'text-size': 12,
+        'text-anchor': 'left'
+      },
+      'paint': {
+        'text-opacity': 0.7
+      },
+    'filter': [
+      '==',
+      'name',
+      'Colors'
+    ]
+    });
+  }
+  onlyColors.forEach(function (color, i) {
+    // add non-styled generatedData data
+    map.addLayer({
+      'id': color,
+      'type': 'fill',
+      'source': 'newdata',
+      'paint': {
+        'fill-color': color
+      },
+      'filter': [
+        '==',
+        'name',
+        color
+      ]
+    });
+  });
+
+  // style all fill colors used
+  var onlyFills = generatedData.allFills.layers;
+  var sorted = {}; // a new object to store our arrays of each value type
+  // console.log(onlyFills);
+
+  Object.keys(onlyFills).forEach(function(key) {
+    var fill = onlyFills[key];
+    var type = fill.type;
+    if (!sorted[type]) { // if there is not key for this type...
+      sorted[type] = [fill]; // make a key with the fill var as the first item in it's array
+    } else { // we do already have this type
+      sorted[type].push(fill); // add the var fill to the key type's array value
     }
   });
 
-  var hideForm = document.getElementById('theForm');
-  var showMap = document.getElementById('map');
-  hideForm.className = 'hidden';
-  showMap.className = '';
-  // console.log('did this happen?');
-  // console.log(generate_data.features);
-  console.log(style);
-
-  mapboxgl.accessToken = accessToken;
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: style,
-    hash: true,
-    zoom: 4.73,
-    center: [-110.9757,47.949]
+  // add the new section label
+  var eachType = [];
+  onlyFills.forEach(function(type, i) {
+    eachType.push(type.type);
   });
-}
+  // for every object in sorted, place a section title layer (find each key)
+  Object.keys(sorted).forEach(function(key, i) {
+    map.addLayer({
+      'id': key,
+      'type': 'symbol',
+      'source': 'newdata',
+      'layout': {
+        'text-field': key,
+        'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+        'text-size': 12,
+        'text-anchor': 'left',
+        'text-transform': 'uppercase'
+      },
+      'paint': {
+        'text-opacity': 0.7
+      },
+      'filter': [
+        '==',
+        'name',
+        key
+      ]
+    });
+    // now add each color from this section
+    sorted[key].forEach(function (fill, i) {
+      // console.log(fill);
+      if(fill.type == key) {
+        // console.log('for types ' + key);
+        // add non-styled generatedData data
+        map.addLayer({
+          'id': fill.name,
+          'type': 'fill',
+          'source': 'newdata',
+          'paint': {
+            'fill-color': fill['fill-color']
+          },
+          'filter': [
+            '==',
+            'name',
+            fill.name
+          ]
+        });
+      }
+    });
+  });
 
-// read style.json url
-function loadJSON(path, success, error)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                if (success)
-                    success(JSON.parse(xhr.responseText));
-            } else {
-                if (error)
-                    error(xhr);
-            }
-        }
-    };
-    xhr.open("GET", path, true);
-    xhr.send();
-}
+  // loop in each object in the array for each section into a fill for the color
+
+//   map.addLayer({
+//   'id': 'sectionFills',
+//   'type': 'symbol',
+//   'source': 'newdata',
+//   'layout': {
+//     'text-field': 'Colors',
+//     'text-font': ['Open Sans Regular', 'Arial Unicode MS Regular'],
+//     'text-size': 12,
+//     'text-anchor': 'left',
+//     'text-transform': 'uppercase'
+//   },
+//   'paint': {
+//     'text-opacity': 0.7
+//   },
+// 'filter': [
+//   '==',
+//   'name',
+//   onlyFills.type
+// ]
+// });
+// onlyFills.forEach(function (fill, i) {
+// // console.log(fill);
+// // add non-styled generatedData data
+// map.addLayer({
+//   'id': fill.name,
+//   'type': 'fill',
+//   'source': 'newdata',
+//   'paint': {
+//     'fill-color': fill['fill-color']
+//   },
+//   'filter': [
+//     '==',
+//     'name',
+//     fill.name
+//   ]
+// });
+// });
+
+  // console.log(sorted);
+
+  // add slider control for zoom
+  map.scrollZoom.disable();
+  var slider = document.getElementById('zoomSlide');
+  var movement = document.getElementById('zoomSlide').value;
+  slider.addEventListener('mouseup', moveZoom);
+
+  // zoom to slider
+  function moveZoom() {
+    map.setZoom(document.getElementById('zoomSlide').value);
+  }
+
+});
+
+
+
+
+
+
 
 
